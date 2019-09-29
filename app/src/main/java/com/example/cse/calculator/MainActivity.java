9ff19cc7da544c5aab1a4345232e0c96c527da47
Manik Hosen <rahathossenmanik@gmail.com>
2019-09-29 12:52:51 +0600

    Basic Buttons

--- app/src/main/java/com/example/cse/calculator/MainActivity.java
+++ app/src/main/java/com/example/cse/calculator/MainActivity.java
@@ -2,12 +2,114 @@ package com.example.cse.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+        public Button btn1, btn2, btn3, btn4, btn5, btn6, btn7, btn8, btn9, btn0, btndec;
+        public Button btnc, btnrt, btnpow, btnadd, btnsub, btnmul, btndiv, btnpercent, btnans;
+        public Button backspace;
+        public TextView input, output;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn0 = findViewById(R.id.btn0);
+        btn1 = findViewById(R.id.btn1);
+        btn2 = findViewById(R.id.btn2);
+        btn3 = findViewById(R.id.btn3);
+        btn4 = findViewById(R.id.btn4);
+        btn5 = findViewById(R.id.btn5);
+        btn6 = findViewById(R.id.btn6);
+        btn7 = findViewById(R.id.btn7);
+        btn8 = findViewById(R.id.btn8);
+        btn9 = findViewById(R.id.btn9);
+        btndec = findViewById(R.id.btndec);
+        btnadd = findViewById(R.id.btnadd);
+        btnsub = findViewById(R.id.btnsub);
+        btnmul = findViewById(R.id.btnmul);
+        btndiv = findViewById(R.id.btndiv);
+        btnans = findViewById(R.id.btnans);
+        btnrt = findViewById(R.id.btnrt);
+        btnpow = findViewById(R.id.btnpow);
+        btnpercent = findViewById(R.id.btnpercent);
+        backspace = findViewById(R.id.btnbacks);
+        input = findViewById(R.id.input);
+        output = findViewById(R.id.output);
+
+        btn0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                input.setText(input.getText()+"0");
+            }
+        });
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                input.setText(input.getText()+"1");
+            }
+        });
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                input.setText(input.getText()+"2");
+            }
+        });
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                input.setText(input.getText()+"3");
+            }
+        });
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                input.setText(input.getText()+"4");
+            }
+        });
+        btn5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                input.setText(input.getText()+"5");
+            }
+        });
+        btn6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                input.setText(input.getText()+"6");
+            }
+        });
+        btn7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                input.setText(input.getText()+"7");
+            }
+        });
+        btn8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                input.setText(input.getText()+"8");
+            }
+        });
+        btn9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                input.setText(input.getText()+"9");
+            }
+        });
+        btndec.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                input.setText(input.getText()+".");
+            }
+        });
+        btnc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                input.setText("");
+            }
+        });
     }
 }
